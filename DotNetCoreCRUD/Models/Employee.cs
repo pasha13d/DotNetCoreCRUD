2fbe6318c6a290f6c8e008dd3221c10057592f75
pasha13d <mahabub13d1995@gmail.com>
2020-09-16 10:05:32 +0600

    Employee Registration

---
+++ DotNetCoreCRUD/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Column(TypeName = "nvarchar(250)")]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        [Display(Name = "Employee Code")]
+        public string EmpCode { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string Position { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        [Display(Name = "Office Location")]
+        public string OfficeLocation { get; set; }
+    }
+}
---
+++ DotNetCoreCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetCoreCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
